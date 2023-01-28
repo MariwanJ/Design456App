82eb6e15ccc3a7886cf3f205a602fca976c89c23
Mariwan <mariwan.jalal@gmail.com>
2023-01-28 23:10:55 +0100

    plane square is shown in a small siz

--- frtk/src/Grid.cpp
+++ frtk/src/Grid.cpp
@@ -8,11 +8,13 @@
 std::shared_ptr<Transform>CreateGrid() {
     auto floor = std::make_shared<Transform>();
     auto bunny_t = std::make_shared<Transform>();
-    bunny_t->Translate(0.9, -0.6, 1.2);
+    bunny_t->Translate(0, 0, 0);
     bunny_t->Scale(5, 5, 5);
-    bunny_t->Rotate(45, 1, 0, 0);
-    bunny_t->Rotate(45, 0, 1, 0);
-    auto bunny = std::make_shared<ObjectShaderNode>(0xAA55AA, 0.02);
+    bunny_t->Rotate(75, 1, 0, 0);
+   // bunny_t->Rotate(0, 0, 1, 0);
+    //bunny_t->Rotate(90, 0, 0, 1);
+
+    auto bunny = std::make_shared<ObjectShaderNode>(0xeA950A, 0.5);
     bunny->SetMesh(std::make_shared<Mesh>("E:/Projects/Design456App/resources/mesh/xy_plane.off"));
     //bunny->SetMesh(std::make_shared<Mesh>("E:/Projects/try things/Scene-Graph-222_Important/src/data/bunny.off"));
     bunny_t->AddNode(bunny);
