1201b7ac0156e09ac6517759ca76a12d29f1923a
Mariwan Jalal <mariwan.jalal@gmail.com>
2025-11-11 18:04:28 +0100

    fix x of label as it was wrong

--- frtk/src/halfedge/fr_shape.cpp
+++ frtk/src/halfedge/fr_shape.cpp
@@ -58,7 +58,7 @@ namespace FR {
 		init(); //This will initializes all Openmesh, vertices, edget ..etc
 		
 		if (m_label) {
-			m_label->pos(glm::vec3(0.0, m_boundBox->maxY(), 0.0f));
+			m_label->pos(glm::vec3(m_boundBox->minX(), m_boundBox->maxY(), 0.0f));
 			m_label->LoadFont();
 		}
 	}
