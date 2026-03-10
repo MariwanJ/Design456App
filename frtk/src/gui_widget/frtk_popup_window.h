6319e728f88dc9a276bfd73b836a50fb4a57a56c
Mariwan Jalal <mariwan.jalal@gmail.com>
2026-03-10 21:16:23 +0100

    still more bugs to fix

--- frtk/src/gui_widget/frtk_popup_window.h
+++ frtk/src/gui_widget/frtk_popup_window.h
@@ -1,33 +1,93 @@
+//
+// This file is a part of the Open Source Design456App
+// MIT License
+//
+// Copyright (c) 2026
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//  Author :Mariwan Jalal    mariwan.jalal@gmail.com
+//
 #ifndef FRTK_POPUP_WINDOW_H
 #define FRTK_POPUP_WINDOW_H
 
 #include <frtk.h>
+#include <frtk_api.h>
 
 #include <GLFW/glfw3.h>
 #include <fr_core.h>
 #include <gui_widget/frtk_grpwidget.h>
 
-namespace FR{
-class Frtk_Popup_Window : public Frtk_GrpWidget{
+namespace FR {
+    class Frtk_Popup_Window : public Frtk_GrpWidget {
+ typedef struct {
+        Frtk_Widget* current;
+        Frtk_Widget* prev;
+        Frtk_Widget* g_underMouse;
+    }global_focus_tracker_popup_t;
+
+  extern   global_focus_tracker_popup_t g_PopupWindfocusedWdgt;
     public:
         Frtk_Popup_Window(int X, int Y, int W, int H, std::string lbl, BOX_TYPE b = FRTK_UP_BOX);
         void setlinkToMain(Fr_Window* window);
- 
+       virtual int Exit();
+ 		static GLFWwindow* getCurrentGLWindow();
+ 		static screenDim_t getScreenDim(void);
+ 		static void deinitializeGlad();
+
+ static Fr_InputEvent_t m_sysEvents;
+        void initSystemEvents();
+static std::shared_ptr<Frtk_Popup_Window> spWindow;
+  ImVec4 clear_color;
+ GLFWcursor* MainWinCursor;  
+        std::vector<CharEvent_t> m_unicodeChars;
+        
         virtual void position(float X, float Y) override;
         virtual void setDecorated(GLFWwindow* w, bool decorated);
         int run(void);
 
     protected:
+
+  		static screenDim_t m_ViewPort;
+        GLFWcursor* cursorHand = nullptr;
+        GLFWcursor* cursorCrosshair = nullptr;
+        static void glfwWindosResize(GLFWwindow* window, int width, int height);
+        static void glfwWindPos(GLFWwindow* window, int pos_x, int pos_y);
+        static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+        static void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+        static void char_callback(GLFWwindow* window, unsigned int codepoint);
+        static void cursor_m_positioncallback(GLFWwindow* win, double xpos, double ypos);
+        static void cursor_enter_callback(GLFWwindow* win, int entered);
+        static void mouse_button_callback(GLFWwindow* win, int button, int action, int mods);
+        static void scroll_callback(GLFWwindow* win, double xoffset, double yoffset);
+        static void joystick_callback(int jid, int events);
+
         virtual int handle(int ev) override;
         virtual void draw() override;
-        GLFWwindow *m_glfpopWindow;
+        GLFWwindow* m_glfpopWindow;
         int gl_version_major;
         int gl_version_minor;
         Fr_Window* m_linkToMainWindow;
         bool s_GLFWpopInitialized;
         bool s_GladInitialized;
-
-
-};
+ 		NVGcontext* m_nvgContext; //NanoVG Context
+		void flush();
+    };
 }
 #endif //FRTK_POPUP_WINDOW_H
---
+++ frtk/src/gui_widget/frtk_popwin_callbacks.cpp
@@ -0,0 +1,228 @@
+//
+// This file is a part of the Open Source Design456App
+// MIT License
+//
+// Copyright (c) 2026
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//  Author :Mariwan Jalal    mariwan.jalal@gmail.com
+//
+
+#include <frtk.h>
+#include <fr_constants.h>
+#include <gui_widget/frtk_popup_window.h>
+#include <fr_core.h>
+
+namespace FR {
+    void Frtk_Popup_Window::glfwWindosResize(GLFWwindow* window, int width, int height)
+    {
+        if (spWindow == nullptr)
+            return;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        pwin->m_ViewPort.size.w = width;
+        pwin->m_ViewPort.size.h = height;
+
+        glfwGetWindowPos(window, &m_ViewPort.pos.x, &m_ViewPort.pos.y); //update even position
+    }
+    void Frtk_Popup_Window::glfwWindPos(GLFWwindow* window, int pos_x, int pos_y)
+    {
+        if (spWindow == nullptr)
+            return;
+        (void)window;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        pwin->m_ViewPort.pos.x = pos_x;
+        pwin->m_ViewPort.pos.y = pos_y;
+    }
+    void Frtk_Popup_Window::framebuffer_size_callback(GLFWwindow* window, int width, int height)
+    {
+        if (spWindow == nullptr)
+            return;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        pwin->m_ViewPort.size.w = width;
+        pwin->m_ViewPort.size.h = height;
+        if (s_GladpopInitialized && s_GLFWpopInitialized) {
+            glViewport(0, 0, width, width);
+        }
+    }
+
+    /*
+
+        window: A pointer to the GLFW window that received the event.
+        key: The keyboard key that was pressed or released.
+        scancode: The system-specific scancode of the key.
+        action: The action that was performed on the key. It can be one of the following values:
+            GLFW_PRESS: The key was pressed.
+            GLFW_RELEASE: The key was released.
+            GLFW_REPEAT: The key was held down and is being repeated.
+        mods: Bit field describing which modifier keys (Shift, Control, Alt, Super) were held down.
+
+    */
+    void Frtk_Popup_Window::keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        if (spWindow == nullptr)
+            return; //do nothing
+        (void)window;
+
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+            spWindow->Exit();
+
+        Frtk_Popup_Window* pwin = spWindow.get();
+        auto& ek = pwin->m_sysEvents.keyB;
+        ek.lastKey = key;
+        ek.scancode = scancode;
+        ek.lastKAction = action;
+        ek.lastMod = mods;
+        ek.shiftDown = (mods & GLFW_MOD_SHIFT) != 0;
+        ek.ctrlDown = (mods & GLFW_MOD_CONTROL) != 0;
+        ek.altDown = (mods & GLFW_MOD_ALT) != 0;
+        ek.superDown = (mods & GLFW_MOD_SUPER) != 0;
+
+        //save key events for pooling later
+        ek.events.push_back({ key, scancode, action, mods });
+    }
+
+    void Frtk_Popup_Window::char_callback(GLFWwindow* window, unsigned int codepoint)
+    {
+        if (!spWindow) return;
+        spWindow->m_unicodeChars.push_back({ codepoint });
+    }
+
+    void Frtk_Popup_Window::joystick_callback(int jid, int events)
+    {
+    }
+    void Frtk_Popup_Window::mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+    {
+        if (!spWindow) return;
+        auto& em = spWindow->m_sysEvents.mouse;
+
+        // Update button state
+        if (button == GLFW_MOUSE_BUTTON_LEFT)       em.L_Down = (action != GLFW_RELEASE);
+        else if (button == GLFW_MOUSE_BUTTON_RIGHT) em.R_Down = (action != GLFW_RELEASE);
+        else if (button == GLFW_MOUSE_BUTTON_MIDDLE) em.M_Down = (action != GLFW_RELEASE);
+
+        em.button = button;
+        em.lastMAction = action;
+        em.lastMod = mods;
+
+        double now = glfwGetTime(); // current time for double-click detection
+
+        // Handle press events and double-click
+        if (action == GLFW_PRESS)
+        {
+            if (button == GLFW_MOUSE_BUTTON_LEFT)
+            {
+                if (now - em.L_lastClickTime <= em.doubleClickThreshold)
+                    em.mouseEvents.push_back(FR_LEFT_DCLICK);  // new enum for left double-click
+                em.L_lastClickTime = now;
+                em.mouseEvents.push_back(FR_LEFT_PUSH);
+            }
+            else if (button == GLFW_MOUSE_BUTTON_MIDDLE)
+            {
+                if (now - em.M_lastClickTime <= em.doubleClickThreshold)
+                    em.mouseEvents.push_back(FR_MIDDLE_DCLICK);
+                em.M_lastClickTime = now;
+                em.mouseEvents.push_back(FR_MIDDLE_PUSH);
+            }
+            else if (button == GLFW_MOUSE_BUTTON_RIGHT)
+            {
+                if (now - em.R_lastClickTime <= em.doubleClickThreshold)
+                    em.mouseEvents.push_back(FR_RIGHT_DCLICK);
+                em.R_lastClickTime = now;
+                em.mouseEvents.push_back(FR_RIGHT_PUSH);
+            }
+        }
+
+        // Handle drag release vs normal release
+        else if (action == GLFW_RELEASE)
+        {
+            if (button == GLFW_MOUSE_BUTTON_LEFT)
+            {
+                if (em.L_Dragging)
+                {
+                    em.L_Dragging = false;
+                    em.mouseEvents.push_back(FR_LEFT_DRAG_RELEASE);
+                }
+                else
+                {
+                    em.mouseEvents.push_back(FR_LEFT_RELEASE);
+                }
+            }
+            else if (button == GLFW_MOUSE_BUTTON_MIDDLE)
+            {
+                if (em.M_Dragging)
+                {
+                    em.M_Dragging = false;
+                    em.mouseEvents.push_back(FR_MIDDLE_DRAG_RELEASE);
+                }
+                else
+                {
+                    em.mouseEvents.push_back(FR_MIDDLE_RELEASE);
+                }
+            }
+            else if (button == GLFW_MOUSE_BUTTON_RIGHT)
+            {
+                if (em.R_Dragging)
+                {
+                    em.R_Dragging = false;
+                    em.mouseEvents.push_back(FR_RIGHT_DRAG_RELEASE);
+                }
+                else
+                {
+                    em.mouseEvents.push_back(FR_RIGHT_RELEASE);
+                }
+            }
+        }
+    }
+    void Frtk_Popup_Window::cursor_m_positioncallback(GLFWwindow* window, double xpos, double ypos)
+    {
+        if (!spWindow)
+            return;
+        (void)window;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        auto& mouse = pwin->m_sysEvents.mouse;
+        mouse.activeX = xpos;
+        mouse.activeY = ypos;
+        mouse.mouseEvents.emplace_back(FR_MOUSE_MOVE);
+    }
+
+    void Frtk_Popup_Window::cursor_enter_callback(GLFWwindow* window, int entered)
+    {
+        if (spWindow == nullptr)
+            return; //do nothing
+        (void)window;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        if (entered) 
+            pwin->m_sysEvents.mouse.mouseEvents.emplace_back(FR_ENTER);
+        else
+            pwin->m_sysEvents.mouse.mouseEvents.emplace_back(FR_LEAVE);
+    }
+
+    void Frtk_Popup_Window::scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+    {
+        if (spWindow == nullptr)
+            return;
+        (void)window;
+        Frtk_Popup_Window* pwin = spWindow.get();
+        auto& m = pwin->m_sysEvents.mouse;
+        m.scrollX = xoffset;
+        m.scrollY= yoffset;
+        m.mouseEvents.emplace_back(FR_SCROLL);
+    }
+}
